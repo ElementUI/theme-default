54db7bd94576eb817db910bb31ab0d9f4a73d5de
wanghongbing <739844323@qq.com>
2020-08-06 11:21:56 +0800

    upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
+var { src, dest, series } = require('gulp');
 var postcss = require('gulp-postcss');
 var cssmin = require('gulp-cssmin');
 var salad = require('postcss-salad')(require('./salad.config.json'));
 
-gulp.task('compile', function() {
-  return gulp.src('./src/*.css')
+function compile() {
+  return src('./src/*.css')
     .pipe(postcss([salad]))
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib'));
-});
-
-gulp.task('copyfont', function() {
-  return gulp.src('./src/fonts/**')
+    .pipe(dest('./lib'));
+}
+function copyfont() {
+  return src('./src/fonts/**')
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib/fonts'));
-});
+    .pipe(dest('./lib/fonts'));
+}
 
-gulp.task('build', ['compile', 'copyfont']);
+exports.default = series(compile, copyfont)
